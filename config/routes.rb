e79419f897fa2e058fb463be37a97b001345bbcb
= <=>
2022-07-19 21:17:26 -0400

    resource jam

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   resources :jams
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
+
   get "page/index"
   root to: "page#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
